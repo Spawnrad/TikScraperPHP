c477d4c2633dc6803a9b64a423c94227b0780d14
Spawnrad <spawnrad@gmail.com>
2023-04-17 20:47:18 +0200

    Update UserAgent

--- src/Constants/UserAgents.php
+++ src/Constants/UserAgents.php
@@ -2,5 +2,5 @@
 namespace TikScraper\Constants;
 
 abstract class UserAgents {
-    const DEFAULT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+    const DEFAULT = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
 }
